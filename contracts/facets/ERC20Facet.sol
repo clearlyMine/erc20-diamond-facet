58adb22131d85e14a972c9aef851ba8e635b15c4
clearly mine <clearlymine@pm.me>
2024-03-27 09:45:37 +0530

    refactor: add foundry fmt config; Refactor

--- contracts/facets/ERC20Facet.sol
+++ contracts/facets/ERC20Facet.sol
@@ -5,7 +5,7 @@ import { LibDiamond } from "../libraries/LibDiamond.sol";
 import { ERC20 } from "../solidstate-solidity/token/ERC20/ERC20.sol";
 
 contract ERC20Facet is ERC20 {
-    function _beforeTokenTransfer(address, address, uint256) internal view override {
-        LibDiamond.enforceIsContractOwner();
-    }
+  function _beforeTokenTransfer(address, address, uint256) internal view override {
+    LibDiamond.enforceIsContractOwner();
+  }
 }
